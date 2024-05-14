198804f7f937b7925bb732b5f773d9cc7fe87b8d
alfonsoHR98 <alfonsohr_98@outlook.com>
2024-05-14 16:44:18 -0600

    chore: Update token cookie configuration in user controller

--- src/controllers/users/user.controller.js
+++ src/controllers/users/user.controller.js
@@ -280,7 +280,7 @@ export const signin = async (req, res, next) => {
     });
 
     res.cookie("token", token, {
-      secure,
+      secure: true,
       SameSite: "None",
       maxAge: 24 * 60 * 60 * 1000,
     });
