ca7e9817be3138ea05e998aef6d5ba34d9ac72b6
alfonsoHR98 <alfonsohr_98@outlook.com>
2024-05-14 15:57:05 -0600

    chore: Update express.urlencoded configuration to not use extended mode

--- src/app.js
+++ src/app.js
@@ -61,7 +61,7 @@ app.use(
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/api", (req, res) => res.json({ message: "Welcome to Niuko SC" }));
 
