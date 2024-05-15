591f5222142cf4028f010c36dd3997d730f022be
alfonsoHR98 <alfonsohr_98@outlook.com>
2024-05-14 23:38:56 -0600

    chore: Update sameSite attribute for token cookie in user controller

--- src/controllers/users/user.controller.js
+++ src/controllers/users/user.controller.js
@@ -281,7 +281,7 @@ export const signin = async (req, res, next) => {
     res.cookie("token", token, {
       //httpOnly: true,
       secure: true,
-      sameSite: "none",
+      sameSite: "Lax",
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
 
